a3dadf5d196bf662992866183b0d1052a7d5cd4c
LoneWolf <saurabhdalakoti07@gmail.com>
2019-04-18 20:37:21 +0530

    Mongo DB added, seprate models file for genre and customer is created

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // we have imported the joi and express module
-const Joi = require('joi');
+const mongoose=require('mongoose');
 const genres=require('./routes/genres');
+const customers=require('./routes/customers');
 const express = require('express');
 const app = express();
-// parsing of the JSON object is not in built in javascript thus we have written line 6
+
+// .connect returns the promise and that need to be handled carefully
+mongoose.connect('mongodb://localhost/vidly')
+    .then(()=> console.log('connected to the mongodb database'))
+    .catch(err=> console.log('could not connnect to the database'));
+
+    // parsing of the JSON object is not in built in javascript thus we have written line 6
 app.use(express.json());
 // any route that starts with /api/genres use router genres which is router from /routes/genres.js
 app.use('/api/genres',genres);
+// any route that ........
+app.use('/api/genres',genres);
+
 // use port environemnt variable of the host ,if it is unavailable then use port 3000
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- routes/genres.js
+++ routes/genres.js
@@ -1,72 +1,57 @@
 // restructering the files to make things more maintainable ,put all the routes in different .js file in route folder
+const mongoose=require('mongoose');
 const express=require('express');
+const {Genre, validate} = require('../models/genre');
 const router=express.Router();
-// genres is the list of dictionary objects
-const genres = [
-    { id: 1, name: 'Action' },  
-    { id: 2, name: 'Horror' },  
-    { id: 3, name: 'Romance' },  
-  ];
-  
-  // req is request and res is response
-  router.get('/', (req, res) => {
+
+// req is request and res is response, and we have made the function as async
+  router.get('/', async(req, res) => {
+    const genres=await Genre.find().sort('name');
     res.send(genres);
   });
   
   // entering a new genre
-  router.post('/', (req, res) => {
+  router.post('/', async(req, res) => {
       // since we cannot trust the users so we are validating the input and hence we are interested in the returned objects' error feildds value so instead of writing const resV= fun() ; const resB=resV['error], we can do tha in one simple way const {error}= fun()
       const { error } = validateGenre(req.body); 
       if (error) return res.status(400).send(error.details[0].message);
   
-    const genre = {
-      id: genres.length + 1,
-      name: req.body.name
-    };
-    genres.push(genre);
+    // genre is being made let because we have to set its value to something that has been set by the database
+    let genre =new Genre({name: req.body.name});
+    genre=await genre.save();
     // return the new genre made
     res.send(genre);
   });
   
   // updation of the genre
-  router.put('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+  router.put('/:id', async (req, res) => {
     const { error } = validateGenre(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+      new: true
+    });
+  
+    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
     
-    genre.name = req.body.name; 
     res.send(genre);
   });
-  
+    
   // deletion of a genre 
-  router.delete('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+  router.delete('/:id', async(req, res) => {
+    const genre =await Genre.findByIdAndRemove(req.params.id);
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
   
     res.send(genre);
   });
   
   // getting the details of particular genre id
-  router.get('/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+  router.get('/:id', async(req, res) => {
+    const genre =await Genre.findById(req.params.id);
+
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
     res.send(genre);
   });
   
-  // this is a validate function 
-  function validateGenre(genre) {
-    // we are using a joi module to validate a string 
-    // syntax is name should be string and minimum of length 3 and it must not be null 
-      const schema = {
-      name: Joi.string().min(3).required()
-    };
-  
-    return Joi.validate(genre, schema);
-  }
-  
+ 
   module.exports=router;
