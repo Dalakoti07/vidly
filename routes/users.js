40c17840f1f35012f89f559eee65666c6347a338
LoneWolf <saurabhdalakoti07@gmail.com>
2019-04-19 14:55:04 +0530

    passwords hashed

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt=require('bcrypt');
 const _=require('lodash');
 const mongoose=require('mongoose');
 const express=require('express');
@@ -14,8 +15,10 @@ const router=express.Router();
     if(user) return res.status(400).send('User already existing');
     // using pick rather then body.get
     user =new User(_.pick(req.body,['name','email','password']));
-    await user.save();
+    const salt=await bcrypt.genSalt(10);
+    user.password=await bcrypt.hash(user.password,salt);// salt is the random string that is prefix or suffix the password    await user.save();
     // lodash has .pick which selects the parms feilds
+    await user.save();
     res.send(_.pick(user,['_id','name','email']));      
 });
 
