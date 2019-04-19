96c32d7f837dd65b8662330bfe9d73e824866b50
LoneWolf <saurabhdalakoti07@gmail.com>
2019-04-19 10:29:45 +0530

    movies api added

--- models/genre.js
+++ models/genre.js
@@ -2,14 +2,16 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 // defining the schema within the params and then the model of genre type
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
+const genreSchema= new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    }
+  });
+
+const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -21,3 +23,4 @@ function validateGenre(genre) {
 
 exports.Genre = Genre; 
 exports.validate = validateGenre;
+exports.genreSchema=genreSchema;
